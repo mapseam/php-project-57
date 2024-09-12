9737d4f76cf4f1413a19a9c0033c2b8db1821439
Oleg Kartashov <mapseam@yandex.ru>
2024-09-12 16:44:52 +0300

    Add symfony/yaml package

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Symfony\Component\Yaml\Yaml;
 
 class DatabaseSeeder extends Seeder
 {
