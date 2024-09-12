151db68b644e163264ef95e0c5dac03b5e08450d
Oleg Kartashov <mapseam@yandex.ru>
2024-09-12 09:57:18 +0300

    Make the TaskStatus views

---
+++ resources/views/task_status/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="grid col-span-full">
+    <h1 class="mb-5">{{ __('task_statuses.create.form_header') }}</h1>
+
+    {{ Form::model($taskStatus, ['route' => 'task_statuses.store', 'class' => 'w-50']) }}
+    <div class="flex flex-col">
+        <div>
+            {{ Form::label('name', __('task_statuses.create.labels.name')) }}
+        </div>
+        <div class="mt-2">
+            {{ Form::text('name', null, ['class' => 'rounded border-gray-300 w-1/3']) }}
+        </div>
+        @error('name')
+        <div class="text-rose-600">{{ $message }}</div>
+        @enderror
+        <div class="mt-2">
+            {{ Form::submit(__('task_statuses.create.buttons.create'), ['class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded']) }}
+        </div>
+    </div>
+    {{ Form::close() }}
+</div>
+@endsection
